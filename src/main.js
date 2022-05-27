ae497de405d3cde195f7e5bd7e75efa4fcd546df
lincongyi <88583354@163.com>
2022-05-27 09:57:26 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 console.log( import.meta.env )
-console.log( import.meta.env.VITE_NAME )
-console.log( import.meta.env.VITE_POSITION )
-createApp(App).mount('#app')
+import router from './router/index'
+createApp(App)
+  .use(router)
+  .mount('#app')
